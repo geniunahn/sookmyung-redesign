0ca2cfc688810f6ce3030b8d1a2a4efe1cd71b82
geniunahn <106502672+geniunahn@users.noreply.github.com>
2022-11-04 13:14:21 +0900

    Add files via upload
    타이틀 이미지 5초마다 전환되는 이벤트 추가
    2단 메뉴 추가

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// main.js
+
+$(function () {
+  let bgArr = [
+    "../image/main-banner1.jpg",
+    "../image/main-banner2.jpg",
+    "../image/main-banner3.jpg",
+  ];
+
+  let bg_img_num = 0;
+
+  // 배경이미지 변경
+
+  setInterval(function () {
+    bg_img_num++;
+    if (bg_img_num >= bgArr.length) {
+      bg_img_num = 0;
+    }
+
+    $("#main-banner .container").css({
+      "background-image": `url(${bgArr[bg_img_num]})`,
+    });
+  }, 5000);
+});
