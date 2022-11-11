68dbc5582a273576f2ebc1dad6b093dc7d8d4ca8
geniunahn <106502672+geniunahn@users.noreply.github.com>
2022-11-11 15:12:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,14 +23,27 @@ $(function () {
   }, 5000);
 });
 
-// 사이트맵 버튼
+//  PC 사이트맵(모바일 GNB) 버튼
 
 $(function () {
   $("#btn-toggle").on("click", function () {
-    $("#site-map-menu").addClass("show");
+    $("#site-map-menu").toggleClass("show");
   });
 
   $("#site-menu-close").on("click", function () {
     $("#site-map-menu").removeClass("show");
   });
+});
+
+$(function () {
+  // GNB 1단계 a 기본 이벤트 제거
+  $("#site-map-menu .site-menu .depth1 > li > div > a").on(
+    "click",
+    function (event) {
+      // 여기다 toggle 기능 추가
+      // 사이트맵 1번 a 태그의 형제 관계에 있는 depth2 ul 태그를 선택하여 클릭할 경우 show 클래스를 추가하여 display:block 으로 보이게 한다!  (당연히 기본적으로 모바일 style 시트에서 depth2 는 display:none 상태여야 한다.)
+      $(this).next().toggleClass("show");
+      event.preventDefault();
+    }
+  );
 });
